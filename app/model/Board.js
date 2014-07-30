756cf903be2def1eb5b88f09ce03bd1cbfe39c2e
trieck <trieck@gmail.com>
2014-07-30 18:54:34 -0400

    Incremental

--- app/model/Board.js
+++ app/model/Board.js
@@ -1,4 +1,62 @@
 Ext.define('Pente.model.Board', {
     extend: 'Ext.data.Model',
-    fields: []
+    fields: [],
+    vectors: null,
+
+    boardSize: function () {
+        return 19;
+    },
+
+    boardEntries: function () {
+        return boardSize() * boardSize();
+    },
+
+    getEntry: function (x, y) {
+        return null;
+    },
+
+    setEntry: function (x, y, type) {
+
+    },
+
+    remove: function (x, y) {
+
+    },
+
+    clear: function () {
+
+    },
+
+    enumEntries: function () {
+
+    },
+
+    empty: function () {
+        return true;
+    },
+
+
+    getVectors: function () {
+        return this.vectors;
+    },
+
+    winner: function (player) {
+        return null;
+    },
+
+    size: function () {
+        return 0;
+    },
+
+    getCaptures: function (x, y, captureVec) {
+        return null;
+    },
+
+    /*
+     * @private
+     */
+    generate: function () {
+
+    }
+
 });
--- app/view/GameView.js
+++ app/view/GameView.js
@@ -2,6 +2,22 @@ Ext.define('Pente.view.GameView', {
     extend: 'Ext.panel.Panel',
     alias: 'widget.gameview',
     bodyStyle: 'background-color:#503200;',
+    items: [
+        {
+            xtype: 'draw',
+            x: 200,
+            y: 100,
+            viewBox: false,
+            items: [{
+                type: 'circle',
+                fill: '#ffc',
+                radius: 100,
+                x: 100,
+                y: 100
+            }],
+            renderTo: document.body
+        }
+    ],
 
     initComponent: function () {
         console.log('The view was initialized!');
