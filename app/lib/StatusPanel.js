79c41a9853991321deb35ce6aae9af4717c87681
trieck <trieck@gmail.com>
2014-08-03 09:03:48 -0400

    Incremental

--- app/lib/StatusPanel.js
+++ app/lib/StatusPanel.js
@@ -1,5 +1,21 @@
 Ext.define('Pente.lib.Statuspanel', {
-    extend: 'Ext.Panel',
+    extend: 'Ext.panel.Panel',
     alias: 'widget.pente-statuspanel',
-    height: 24
+
+    bbar: Ext.create('Pente.lib.StatusBar', {
+        id: 'pente-status',
+
+        // defaults to use when the status is cleared:
+        defaultText: 'Default status text',
+        defaultIconCls: 'default-icon',
+
+        // values to set initially:
+        text: 'Ready',
+        iconCls: 'ready-icon',
+
+        // any standard Toolbar items:
+        items: [{
+            text: 'A Button'
+        }, '-', 'Plain Text']
+    })
 });
