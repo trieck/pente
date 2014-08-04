a4719a5bec3a9db70eeda6faf1f63c76d7c8b68b
trieck <trieck@gmail.com>
2014-08-04 19:13:46 -0400

    bug fixes and board drawing

--- app/lib/Table.js
+++ app/lib/Table.js
@@ -3,8 +3,15 @@ Ext.define('Pente.lib.Table', {
 	alias: 'widget.pente-table',
 	requires: [ 'Pente.model.Board' ],
 	viewBox: false,
-	items: [
-		{
+	items: [],
+	initComponent: function () {
+		var dims = Pente.model.Board.dimensions();
+		var cx = Pente.model.Board.squareSize;
+		var cy = Pente.model.Board.squareSize;
+		var x = Pente.model.Board.cxBorder;
+		var y = Pente.model.Board.cyBorder;
+
+		this.items.push({
 			type: 'rect',
 			width: Pente.model.Board.width(),
 			height: Pente.model.Board.height(),
@@ -12,28 +19,36 @@ Ext.define('Pente.lib.Table', {
 			stroke: '#c0c0c0',
 			x: Pente.model.Board.cxBorder,
 			y: Pente.model.Board.cyBorder
-		}
-	],
-	initComponent: function () {
-		var dims = Pente.model.Board.dimensions();
-		var cx = Pente.model.Board.squareSize;
-		var cy = Pente.model.Board.squareSize;
-		var x = Pente.model.Board.cxBorder;
-		var y = Pente.model.Board.cyBorder;
-		var ptStart = Ext.create('Ext.util.Point', x, y);
+		});
 
 		// draw vertical lines
-		while (ptStart.x <= dims.right) {
-			var ptEnd = Ext.create('Ext.util.Point', ptStart.x, (dims.bottom + y) - 1);
+		var ptStart = Ext.create('Ext.util.Point', x + cx, y);
+		while (ptStart.x < dims.right) {
+			var ptEnd = Ext.create('Ext.util.Point', ptStart.x, dims.bottom + y);
 			var path = Ext.String.format("M{0} {1} L{2} {3}", ptStart.x, ptStart.y, ptEnd.x, ptEnd.y);
 			var item = {
 				type: 'path',
 				path: path,
 				stroke: '#c0c0c0',
-				'stroke-width': .5
+				'stroke-width': 1
 			};
 			this.items.push(item);
 			ptStart.x += cx;
+		}
+
+		// Draw horizontal lines
+		ptStart = Ext.create('Ext.util.Point', x, y + cy);
+		while (ptStart.y < dims.bottom) {
+			ptEnd = Ext.create('Ext.util.Point', dims.right + x, ptStart.y);
+			path = Ext.String.format("M{0} {1} L{2} {3}", ptStart.x, ptStart.y, ptEnd.x, ptEnd.y);
+			item = {
+				type: 'path',
+				path: path,
+				stroke: '#c0c0c0',
+				'stroke-width': 1
+			};
+			this.items.push(item);
+			ptStart.y += cy;
 		}
 
 		this.callParent(arguments);
