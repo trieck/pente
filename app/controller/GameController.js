f7f489bb3de7f4402756563ca5896c4eee8fc20a
trieck <trieck@gmail.com>
2014-08-04 20:43:46 -0400

    Incremental

--- app/controller/GameController.js
+++ app/controller/GameController.js
@@ -1,20 +1,26 @@
 Ext.define('Pente.controller.GameController', {
-    extend: 'Ext.app.Controller',
-    models: [ 'Pente.model.Board' ],
-    views: [ 'Pente.view.View' ],
+		extend: 'Ext.app.Controller',
+		models: [ 'Pente.model.Board' ],
+		views: [ 'Pente.view.View' ],
 
-    init: function () {
-        this.control({
-            'view': {
-                render: this.onViewRendered
-            }
-        });
-    },
+		init: function () {
+			this.control({
+				'pente-view': {
+					render: this.onViewRendered
+				}
+			});
+		},
 
-    onViewRendered: function (view) {
-        view.body.on('click', this.onClicked);
-    },
+		onViewRendered: function (view) {
+			view.body.on('click', this.onClicked);
+		},
 
-    onClicked: function () {
-    }
-});
+		onClicked: function (e) {
+			var event = e.browserEvent;
+			var x = event.offsetX ? event.offsetX : event.layerX;
+			var y = event.offsetY ? event.offsetY : event.layerY;
+			var b = Pente.model.Board.ptOnBoard(x, y);
+			Ext.Msg.alert("" + b);
+		}
+	}
+);
--- app/model/Board.js
+++ app/model/Board.js
@@ -1,69 +1,76 @@
 Ext.define('Pente.model.Board', {
-    extend: 'Ext.data.Model',
-    statics: {
-        boardSize: 19,
-        cxBorder: 20,
-        cyBorder: 20,
-        cxOffset: 2,
-        cyOffset: 2,
-        cxSquares: 18,
-        cySquares: 18,
-        squareSize: 21,
-        boardEntries: 19 * 19,
-        width: function () {
-            return this.squareSize * this.cxSquares;
-        },
-        height: function () {
-            return this.squareSize * this.cySquares;
-        },
-        dimensions: function () {
-            var right = this.width();
-            var bottom = this.height();
-            return new Ext.util.Region(0, right, bottom, 0);
-        },
-        boundingRect: function () {
-            var dims = this.dimensions();
-            dims.right += this.cxBorder * 2;
-            dims.bottom += this.cyBorder * 2;
-            return dims;
-        }
-    },
+	extend: 'Ext.data.Model',
+	statics: {
+		boardSize: 19,
+		cxBorder: 20,
+		cyBorder: 20,
+		cxOffset: 2,
+		cyOffset: 2,
+		cxSquares: 18,
+		cySquares: 18,
+		squareSize: 21,
+		boardEntries: 19 * 19,
+		width: function () {
+			return this.squareSize * this.cxSquares;
+		},
+		height: function () {
+			return this.squareSize * this.cySquares;
+		},
+		dimensions: function () {
+			var right = this.width();
+			var bottom = this.height();
+			return new Ext.util.Region(0, right, bottom, 0);
+		},
+		boundingRect: function () {
+			var dims = this.dimensions();
+			dims.right += this.cxBorder * 2;
+			dims.bottom += this.cyBorder * 2;
+			return dims;
+		},
+		ptOnBoard: function (x, y) {
+			var dims = this.dimensions().copy();
+			dims.adjust(this.cyBorder, this.cyBorder, this.cyBorder, this.cxBorder);
+			if (x >= dims.x && y >= dims.y && x < dims.right && y < dims.bottom)
+				return true;
+			return false;
+		}
+	},
 
-    vectors: null,
+	vectors: null,
 
-    getEntry: function (x, y) {
-        return null;
-    },
+	getEntry: function (x, y) {
+		return null;
+	},
 
-    setEntry: function (x, y, type) {
+	setEntry: function (x, y, type) {
 
-    },
+	},
 
-    remove: function (x, y) {
+	remove: function (x, y) {
 
-    },
+	},
 
-    clear: function () {
+	clear: function () {
 
-    },
+	},
 
-    enumEntries: function () {
+	enumEntries: function () {
 
-    },
+	},
 
-    empty: function () {
-        return true;
-    },
+	empty: function () {
+		return true;
+	},
 
-    winner: function (player) {
-        return null;
-    },
+	winner: function (player) {
+		return null;
+	},
 
-    getCaptures: function (x, y, captureVec) {
-        return null;
-    },
+	getCaptures: function (x, y, captureVec) {
+		return null;
+	},
 
-    generate: function () {
-    }
+	generate: function () {
+	}
 
 });
