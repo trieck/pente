24d0064960024926102dfeccaeccf556fd4c8a3c
trieck <trieck@gmail.com>
2014-08-08 17:43:08 -0400

    layout changes

--- app/lib/Frame.js
+++ app/lib/Frame.js
@@ -1,18 +1,21 @@
 Ext.define('Pente.lib.Frame', {
-    extend: 'Ext.window.Window',
-    requires: [ 'Pente.lib.Toolbar', 'Pente.lib.Statuspanel' ],
-    frame: true,
-    closable: false,
-    title: 'Pente for ExtJS',
-    border: true,
-    items: [
-        { xtype: 'pente-toolbar' },
-        { xtype: 'pente-view' },
-        { xtype: 'pente-statuspanel' }
-    ],
-    resizable: false,
+	extend: 'Ext.window.Window',
+	requires: [ 'Pente.lib.Toolbar', 'Pente.lib.Statuspanel' ],
+	frame: true,
+	closable: false,
+	title: 'Pente for ExtJS',
+	border: true,
+	items: [
+		{ xtype: 'pente-view' }
+	],
+	dockedItems: [
+		{ xtype: 'pente-toolbar' }
+	],
+	bbar: { xtype: 'pente-statuspanel' },
+	resizable: false,
 
-    initComponent: function () {
-        this.callParent(arguments);
-    }
-});
+	initComponent: function () {
+		this.callParent(arguments);
+	}
+})
+;
