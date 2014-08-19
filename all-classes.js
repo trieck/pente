d2197a93972f2b7865243cc1d40a09d1b1d2e661
trieck <trieck@gmail.com>
2014-08-18 22:11:37 -0400

    Update build

--- all-classes.js
+++ all-classes.js
@@ -127,6 +127,11 @@ Ext.define('Pente.view.BoardComponent', {
     },
 
     setGridColor: function (color) {
+        var items = Ext.query('path');
+        var nlength = items.length;
+        for (var i = 0; i < nlength; ++i) {
+            Ext.fly(items[i]).setStyle('stroke', color);
+        }
     }
 });
 /**
